88249f8d3c2bdeabc5bb652d26930b9566074be4
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2019-09-14 11:06:32 +0300

    working on shop collection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
-import HomePage from "./pages/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
       </div>
     );
   }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+class ShopPage extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div>
+        {collections.map(({ id, ...otherPreviewProps }) => (
+          <div key={id}>
+            <CollectionPreview {...otherPreviewProps} />{" "}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
