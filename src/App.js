df912f905bccac860902d2e95140d342f200be9f
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2020-01-22 23:01:32 +0200

    added all checkout page functionality with add and remove item

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Checkout from './pages/checkout/checkout.component';
 import SignOnAndSignOutPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.components';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
@@ -57,6 +58,7 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={Checkout} />
           <Route
             exact
             path='/signin'
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,5 @@
 import CartActionType from './cart.types';
-import { addItemToCart } from "./cart.utils";
-
+import {addItemToCart, removeItemFromCart} from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -12,13 +11,25 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionType.TOGGLE_CART_HIDDEN:
       return {
         ...state,
-        hidden : !state.hidden
+        hidden: !state.hidden
       };
 
     case CartActionType.ADD_ITEM:
       return {
         ...state,
-        cartItems : addItemToCart(state.cartItems, action.payload)
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+
+    case CartActionType.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+      };
+
+    case CartActionType.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
       };
 
     default:
