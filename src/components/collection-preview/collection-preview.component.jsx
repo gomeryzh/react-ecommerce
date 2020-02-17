3f482f3119880da8431eb26e6ad7643e6b59d48c
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2020-02-17 21:32:23 +0200

    redux thunk and Link to collection page added

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
 import CollectionItem from "../collection-item/collection-item.component";
+
 import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
+    <Link to={`/shop/${title.toLowerCase()}`} className="title">{title.toUpperCase()}</Link>
     <div className="preview">
       {items
         .filter((item, index) => index < 4)
